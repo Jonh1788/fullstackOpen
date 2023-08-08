fff3a2f240c96078425bc0d9cef476c362366a1c
Jonh1788 <110953238+Jonh1788@users.noreply.github.com>
2023-08-08 19:48:37 -0300

    Update App.js

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,4 +1,32 @@
-const App = () => <p>Hello world</p>
+import { useState } from 'react'
 
+const Titulo = ({texto}) => <h1>{texto}</h1>
+const Botao = ({handleClique, texto}) => {
+    return(
+        <button onClick={handleClique}>{texto}</button>
+    )
+}
+
+const StatisticLine = ({texto, valor}) => <p>{texto} {valor}</p>
+
+const App = () => {
+  // salve os cliques de cada botão em seu próprio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  
+
+  return (
+    <div>
+      <Titulo texto={"give feedback"}/>
+      <Botao texto={"good"}/>
+      <Botao texto={"neutral"}/>
+      <Botao texto={"bad"}/>
+      <Titulo texto={"statistics"}/>
+      <StatisticLine texto={"good"} valor={good}/>
+    </div>
+  )
+}
 
 export default App
