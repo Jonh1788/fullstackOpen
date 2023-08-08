150ac2d6fd615f4202efe88b5120801193ef77d1
Jonh1788 <110953238+Jonh1788@users.noreply.github.com>
2023-08-08 15:48:32 -0300

    Update App.js

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,8 +1,18 @@
-const Header = ({ course }) => {
-  return (
-    <h1>{course}</h1>
-  )
+const Header = ({ course }) => <h1>{course}</h1>
+const Part = ({text, exercise}) => <p>{text} {exercise}</p> 
+
+const Content = (props) => {
+ return( 
+<>
+<Part text={props.text} exercise={props.exercise}/>
+<Part text={props.text1} exercise={props.exercise1}/>
+<Part text={props.text2} exercise={props.exercise2}/>
+</>
+ )
 }
+
+const Total = ({total}) => <p>Number of exercises {total}</p>
+
 
 const App = () => {
   const course = 'Desenvolvimento de aplicação Half Stack'
@@ -12,20 +22,13 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'Estado de um componente'
   const exercises3 = 14
+  const total = exercises1 + exercises2 + exercises3
 
   return (
     <div>
       <Header course={course}/>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content text={part1} exercise={exercises1} text1={part2} exercise1={exercises2} text2={part3} exercise2={exercises3}/>
+      <Total total={total}/>
     </div>
   )
 }
