35b4768d2dc029b73b452548e679fb378adf16d0
Jonh1788 <110953238+Jonh1788@users.noreply.github.com>
2023-08-09 16:43:18 -0300

    Update App.js

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,7 +7,39 @@ const Botao = ({handleClique, texto}) => {
     )
 }
 
-const StatisticLine = ({texto, valor}) => <p>{texto} {valor}</p>
+const StatisticLine = ({texto, valor}) => {
+
+ return(
+  <tbody>
+    <tr>
+      <td>{texto}</td>
+      <td>{valor}</td>
+    </tr>
+  </tbody>
+
+ ) 
+}
+
+const Statistic = ({valor, total}) => {
+  if(total === 0)
+  {
+    return(
+      <p>No feedback given</p>
+    )
+  }
+  return(
+    <table>
+    <StatisticLine texto={valor[0].name} valor={valor[0].valor}/>
+    <StatisticLine texto={valor[1].name} valor={valor[1].valor}/>
+    <StatisticLine texto={valor[2].name} valor={valor[2].valor}/>
+    <StatisticLine texto={valor[3].name} valor={valor[3].valor}/>
+    <StatisticLine texto={valor[4].name} valor={valor[4].valor}/>
+    <StatisticLine texto={valor[5].name} valor={valor[5].valor}/>
+    </table>
+    
+    
+  )
+}
 
 const App = () => {
   // salve os cliques de cada botão em seu próprio estado
@@ -17,14 +49,60 @@ const App = () => {
 
   
 
+  const aumentarGoodEmUm = () => {
+    setGood(good + 1)
+  }
+
+  const aumentarNeutralEmUm = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const aumentarBadEmUm = () => {
+    setBad(bad + 1)
+  }
+
+  
+  const total = good + bad + neutral
+  const positive = (good/total)*100
+  const average = (good-bad)/total
+
+  const statistics = [
+    {
+      name: "good",
+      valor: good
+    },
+    {
+      name: "neutral",
+      valor: neutral
+    },
+    {
+      name: "bad",
+      valor: bad
+    },
+    {
+      name: "all",
+      valor: total
+    },
+    {
+      name: "average",
+      valor: average.toFixed(1)
+    },
+    {
+      name: "positive",
+      valor: positive.toFixed(1) + "%"
+    }
+    
+
+  ]
+
   return (
     <div>
       <Titulo texto={"give feedback"}/>
-      <Botao texto={"good"}/>
-      <Botao texto={"neutral"}/>
-      <Botao texto={"bad"}/>
+      <Botao handleClique={aumentarGoodEmUm} texto={"good"}/>
+      <Botao handleClique={aumentarNeutralEmUm} texto={"neutral"}/>
+      <Botao handleClique={aumentarBadEmUm} texto={"bad"}/>
       <Titulo texto={"statistics"}/>
-      <StatisticLine texto={"good"} valor={good}/>
+      <Statistic valor={statistics} total={total}/>
     </div>
   )
 }
